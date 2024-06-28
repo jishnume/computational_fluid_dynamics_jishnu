6e233a6437fd7a2fd760ec860456738a204da4cb
Jishnu Goswami <goswamijishnume@gmail.com>
2024-06-28 22:25:31 +0530

    Added a basic c++ code to demonstrate creating variables

---
+++ start_with_c++/defining_variables.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std ;
+
+int main()
+{
+
+	// I am explaining datatypes and variables in c++
+
+	// ------------------------------------------------------
+	//                    Datatypes in c++                  -
+	// ------------------------------------------------------
+
+	int age = 26 ;                             // integer datatype
+
+	char gender = 'M' ;                         // character datatype
+
+	// char name[10] = "Jishnu" ;                      // String datatype
+
+	bool is_older_than18 = true ;               // boolean datatype
+
+	float todays_temperature = 36.5 ;           // floating point datatype
+
+	double radius_of_earth = 6400000 ;          //double floating point datatype
+
+	cout << "we are learning c++ "<< endl ;
+	
+	return 0 ;
+}
